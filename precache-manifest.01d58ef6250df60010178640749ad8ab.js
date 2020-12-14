b9414ce3650f5e8be96700e4c81555db94f69adf
Paranoidal97 <19rams97@gmail.com>
2020-12-14 18:40:50 +0100

    Updates

---
+++ precache-manifest.01d58ef6250df60010178640749ad8ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05378283668e11a77c67afef7763279",
+    "url": "/Virtual-Breathalyzer/index.html"
+  },
+  {
+    "revision": "342fbef8fbaa5705206b",
+    "url": "/Virtual-Breathalyzer/static/css/main.8b6dae69.chunk.css"
+  },
+  {
+    "revision": "7cf263b4a9afd0c5e8e0",
+    "url": "/Virtual-Breathalyzer/static/js/2.16ba4900.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Virtual-Breathalyzer/static/js/2.16ba4900.chunk.js.LICENSE"
+  },
+  {
+    "revision": "342fbef8fbaa5705206b",
+    "url": "/Virtual-Breathalyzer/static/js/main.1395942c.chunk.js"
+  },
+  {
+    "revision": "81caa139560ad342e21e",
+    "url": "/Virtual-Breathalyzer/static/js/runtime-main.21c579b5.js"
+  },
+  {
+    "revision": "5de989795a561e0d5c2df21254082426",
+    "url": "/Virtual-Breathalyzer/static/media/image.5de98979.svg"
+  }
+]);
